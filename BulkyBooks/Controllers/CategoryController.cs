fba6a96c9d9f971c91ceb43f9b9cc38e8611b0cf
Komron <komronabdujabbor@gmail.com>
2023-08-14 19:17:48 +0500

    category controllerini qo'shdim

--- BulkyBooks/Controllers/CategoryController.cs
+++ BulkyBooks/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿global using BulkyBooks.Data;
+global using BulkyBooks.Models;
+global using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBooks.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly BulkyBookDbContext _db;
+        public CategoryController(BulkyBookDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
         }
     }
 }
