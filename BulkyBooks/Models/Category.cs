0f5cfcf974f9c49ecb221f5961517b43e6abde78
Komron <komronabdujabbor@gmail.com>
2023-08-15 18:37:21 +0500

    Range atributi qo'shildi

--- BulkyBooks/Models/Category.cs
+++ BulkyBooks/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBooks.Models
 {
@@ -8,6 +9,8 @@ namespace BulkyBooks.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage ="Display order must be between 1 and 100 only!!")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; }= DateTime.Now;
     }
