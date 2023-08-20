93d2e61787b64d7bc1958c8a1a6558dd71459bc0
Komron <komronabdujabbor@gmail.com>
2023-08-20 20:37:08 +0500

    added post method

---
+++ firstdmo/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using firstdemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace firstdemo.Controllers
+{
+    public class BookController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Show()
+        {
+            return "";
+        }
+        [HttpPost]
+        public string Index(Book book)
+        {
+            return $"{book.Id}, {book.Title}, {book.Description}, {book.Phone}, {book.Count}, {book.Email}";
+        }
+    }
+}
