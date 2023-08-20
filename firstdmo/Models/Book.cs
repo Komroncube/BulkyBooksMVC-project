8f1cf254d0384c92faf0a7fc585a14af1400c5fa
Komron <komronabdujabbor@gmail.com>
2023-08-20 20:36:58 +0500

    book model was modified

--- firstdmo/Models/Book.cs
+++ firstdmo/Models/Book.cs
@@ -4,5 +4,9 @@
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public string Description { get; set; }
+        public string Phone { get; set; }
+        public int Count { get; set; }
+        public string Email { get; set; }
     }
 }
