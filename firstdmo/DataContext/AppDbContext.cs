9e41fe402e58d8e6d9c094bbd37b524e72264ace
Komron <komronabdujabbor@gmail.com>
2023-08-21 18:57:14 +0500

    company degan model uchun database yaratildi

---
+++ firstdmo/DataContext/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿namespace firstdemo.DataContext
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Company> Companies { get; set; }
+    }
+}
