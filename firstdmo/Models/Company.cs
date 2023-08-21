09352e6857d7160c884cd9ed64fae988d9d3d150
Komron <komronabdujabbor@gmail.com>
2023-08-21 18:27:16 +0500

    company modeli yaratildi

---
+++ firstdmo/Models/Company.cs
@@ -0,0 +1,10 @@
+﻿namespace firstdemo.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; } 
+        public string Contact { get; set; }
+    }
+}
