1d588c9da3afe9d801e73ef22f9e8b13355a1086
Komron <komronabdujabbor@gmail.com>
2023-08-21 17:43:41 +0500

    ActionResult created

---
+++ firstdmo/HtmlResult.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace firstdemo
+{
+    public class HtmlResult:IActionResult
+    {
+        string htmlCode;
+
+        public HtmlResult(string html)=> htmlCode = html;
+        public async Task ExecuteResultAsync(ActionContext actionContext)
+        {
+            string fullHtmlCode= @$"<!DOCTYPE html>
+            <html>
+                <head>
+                    <title>METANIT.COM</title>
+                    <meta charset=utf-8 />
+                </head>
+                <body>{htmlCode}</body>
+            </html>";
+            await actionContext.HttpContext.Response.WriteAsync(fullHtmlCode);
+
+        }
+    }
+}
