52b1a1e78df32fdb3a03fdefed8a79dc17260ac9
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-08 16:05:36 +0500

    removed project

--- firstdmo/GlobalUsing.cs
+++
@@ -1,9 +0,0 @@
-﻿global using firstdemo.Models;
-global using firstdemo.Interfaces;
-global using firstdemo.Controllers;
-global using firstdemo.DataContext;
-global using Microsoft.AspNetCore.Http;
-global using Microsoft.AspNetCore.Mvc;
-global using firstdemo.Services;
-global using Microsoft.EntityFrameworkCore;
-
